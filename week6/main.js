4293be3d7da8b4ca954e6fe36e6b7b6d85e8d36f
Danny <dannyplaysthebass@gmail.com>
2021-02-22 22:55:41 -0800

    First push per instructions on project 3

---
+++ week6/main.js
@@ -0,0 +1,54 @@
+// how to USE a function: functionName();
+console.log("Whats cooking?");
+console.log("Nothing good, at least yet!");
+
+// Variables
+
+// Defining a Variable in our programs.
+var VariableNameHere;
+
+myFirstVariable = "I like to cook.";
+mySecondVariable = "What do you like?" ;
+
+console.log(myFirstVariable);
+console.log(mySecondVariable);
+
+// The 3 most common Datatypes
+
+// Strings
+"I like to cook.";
+"What do you like?";
+
+
+'What do you like?';
+
+// Number
+1;
+2000;
+18.9;
+-34;
+
+// Booleans
+true;
+false;
+
+// Mathematical Operations
+10 + 10; //20
+
+
+var someMath = 10 + 8459;
+
+console.log(someMath);
+
+var currentCTemp = 60;
+
+var cToF = ( (currentCTemp * (9/5) ) + 32) ;
+
+console.log(cToF);
+
+// Logical Operators
+
+// --Means 'Equal to'
+
+(100 == 100); // becomes a true
+(101 == 100); // becomes a false
